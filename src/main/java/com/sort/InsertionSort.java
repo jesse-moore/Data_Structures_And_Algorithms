a5166dfe689219cdf6f819e154de2b18ba260be0
Jesse <moore.jesse@gmail.com>
2021-04-03 18:32:17 -0500

    added insertion sort

--- src/main/java/com/sort/InsertionSort.java
+++ src/main/java/com/sort/InsertionSort.java
@@ -5,11 +5,13 @@ import java.util.Arrays;
 public class InsertionSort {
     public static void sort(int[] arr){
         for (int i = 1; i < arr.length; i++) {
-            for (int j = i; j > 0; j--) {
+            for (int j = i; j > 0 && arr[j]<arr[j-1]; j--) {
                 if(arr[j]<arr[j-1]){
                     swap(arr, j-1, j);
                 }
+                System.out.println(j);
             }
+            System.out.println(Arrays.toString(arr));
         }
     }
 
